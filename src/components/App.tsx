d91b883ee5186d878c8dd1f5393cacd4fc5c2ce5
joel <joelnqwork@gmail.com>
2024-08-13 19:57:47 +0200

    navbar has been created

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import '../assets/css/App.css'
-import { Link } from 'react-router-dom'
+import NavBar from './NavBar'
+import SideBar from './SideBar'
 interface productSchema {
   category: string,
   description: string,
@@ -16,6 +17,9 @@ interface productSchema {
 
 function App() {
   const [products, setProducts] = useState<productSchema[]>([])
+  const [categories, setCategories] = useState<string[]>([])
+  const [openSideBar, setOpenSideBar] = useState<boolean>(false)
+  const [cartProducts, setCartProducts] = useState<productSchema[]>([])
   const apiURL = import.meta.env.VITE_API_URL
   useEffect(() => {
     fetch(apiURL)
@@ -23,25 +27,37 @@ function App() {
       .then(res => setProducts(res))
   }, [apiURL])
 
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products/categories')
+    .then(res => res.json())
+    .then(res => setCategories(res))
+  },[])
+
   const handleProductInfo = (productID: number) => {
     const url = `${window.location.origin}/product/${productID}`;
     window.open(url, '_blank',);
   }
+  
+  const handleAddProduct = (product: productSchema) => {
+    setCartProducts(prevState => [...prevState, product])
+  }
   return (
     <>
+    <NavBar categories={categories} onHandleSideBar={setOpenSideBar}/>
       <main className='app'>
         {products.map((product) => {
           return (
             <div key={product.id} className='product-wrapper'>
               <button onClick={()=> handleProductInfo(product.id)} className='button-image'><img src={product.image} alt={product.title} className='product-img' /></button>
               <p>{product.title}</p>
-              <p>{product.category}</p>
-              <p>{product.price}</p>
-              <button>add to cart</button>
+              <div className='price-wrapper'>
+              <p>€{product.price}</p>
+              <button onClick={()=> handleAddProduct(product)} >add to cart</button>
+              </div>
             </div>
-            
           )
         })}
+        {openSideBar &&  <SideBar products={cartProducts} onCloseSideBar={setOpenSideBar}/>}
       </main>
     </>
   )
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,24 @@
-import { Link } from "react-router-dom";
 import React from "react";
+import '../assets/css/NavBar.css'
+interface NavBarProps {
+    categories: string[]
+    onHandleSideBar: (status: boolean) => void
+}
 
-const NavBar: React.FC = () => {
-    return(
+const NavBar: React.FC<NavBarProps> = ({ categories, onHandleSideBar }) => {
+    console.log(categories);
+
+    return (
         <nav>
-            
+            <ul className="categories">
+                {categories.map((catergory) => {
+                    return (
+                        <li key={catergory}>{catergory}</li>
+                    )
+                })}
+            </ul>
+            <p>FakeStore</p>
+            <img src='../../src/assets/images/shopping-cart.png' alt="shopping cart" onClick={() => onHandleSideBar(true)} className="shopping-cart" />
         </nav>
     )
 }
