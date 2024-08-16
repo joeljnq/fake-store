1fdaf848bcf88498438dd012847c6266e8289d33
joel <joelnqwork@gmail.com>
2024-08-16 09:24:47 +0200

    the cart Button has been added

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './components/App'
 import './assets/css/index.css'
 import ProductInfo from './components/ProductInfo'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ShoppingCart from './components/ShoppingCart'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />} />
       <Route path='/product/:productID' element={<ProductInfo />} />
+      <Route path='cart' element={<ShoppingCart />}></Route>
     </Routes>
     </BrowserRouter>
 
